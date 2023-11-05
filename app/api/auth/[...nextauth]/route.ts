e7456e07f6093ed09a3c941a741867240db757b9
ELOUAIZI Ayoub <ayoubelouaizi@gmail.com>
2023-11-05 09:21:52 +0100

    login using credentials with  next auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,52 @@
+import { users } from "@/helpers/constants";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+// import GithubProvider from "next-auth/providers/github"; //we are not using github provider in this project.
+
+const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter the email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter the password",
+        },
+      },
+
+      async authorize(credentials) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        //! this is means the credentials not correct.
+        if (!credentials || !credentials.email || !credentials.password)
+          return null;
+
+        //? we will search if the user from the credentials exists in our fake database existing in the helper
+        // we are also using the function find in javascript to do the search.
+        const user = users.find((user) => user.email === credentials.email);
+        //? check if the user actually exists and the password is correct and return it.
+        if (user && user.password === credentials.password) {
+          return user;
+        }
+        // Return null if user if not found or password is incorrect.
+        return null;
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ helpers/constants.ts
@@ -0,0 +1,16 @@
+// in Our cas bacause we just want to test the the next auth as first try
+//! this si going to be our data base to make the process a bit simpler
+export const users = [
+  {
+    id: 1,
+    name: "ayoub elouaizi",
+    email: "ayoub@example.com",
+    password: "000000",
+  },
+  {
+    id: 2,
+    name: "ayoub elouaizi",
+    email: "amine@example.com",
+    password: "000000",
+  },
+];
